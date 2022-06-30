04130e1bf5312427dfe41c8fe9a212b5b07e6c9e
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-06-30 14:29:30 +0900

    basicOperate

--- FREECAMP_31H/12_basciOperate.cpp
+++ FREECAMP_31H/12_basciOperate.cpp
@@ -16,7 +16,7 @@ int main(){
     result = number1 / number2;
     std::cout << "result : " << result << std::endl; //result : 2 -> 소수점 버리는 연산 
 
-    result = number1 % number2;
+    result = number1 % number2; //result : 1 --> 소수점만 가져오는 연산을 한다
     std::cout << "result : " << result << std::endl;
 
     return 0;
