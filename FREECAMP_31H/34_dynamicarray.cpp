273c47c1e885c38e3f141918d2acd5cd46040675
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-07-27 14:06:50 +0900

    limitation of dynamic array

--- FREECAMP_31H/34_dynamicarray.cpp
+++ FREECAMP_31H/34_dynamicarray.cpp
@@ -13,4 +13,26 @@ int main(){
 
     delete[] p_salaries;
     p_salaries = nullptr;
+
+    //Static array Vs dynamic array
+    //Static array is fixed size and can not be changed.
+    //Dynamic array is dynamic size and can be changed.
+
+    int scores[10]{1,2,3,4,5,6,7,8,9,10}; // static array 
+
+    std:: cout << "scores size" << sizeof(scores) << std::endl;
+    for(auto s: scores){
+        std::cout << s << std::endl;
+    }
+
+    int * p_scores1 {new int[10]{1,2,3,4,5,6,7,8,9,10}}; // dynamic array
+                                                        // live on the heap
+    // not working sizeof and for in dynamic array
+    /*
+    std:: cout << "p_scores1 size" << sizeof(p_scores1) << std::endl;
+    for(auto s: p_scores1){
+        std::cout << s << std::endl;
+    }
+    */
+    return 0;
 }
