3a70df9bdf3b371291fa30abcc096d08309e2d6d
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-07-27 13:48:23 +0900

    dynamic array assignment

---
+++ FREECAMP_31H/34_dynamicarray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    size_t size{10};
+
+    double *p_salaries { new double[size]}; // garbage data 
+    int *p_student {new(std::nothrow) int[size]{} }; // all data 0
+    double *p_score {new(std::nothrow) double[size]{1,2,3,4,5} }; // after 5 all data is 0
+
+    for( size_t i{0}; i<size; i++){
+        std::cout << "value : " << p_score[i] << ":" << *(p_score+i) << std::endl;
+    }
+}
